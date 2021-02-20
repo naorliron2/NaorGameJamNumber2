c1b77adeb9ecb4a8eaa7292c1a4c30f2491573f4
naorr liron <naorliron2@gmail.com>
2021-02-20 02:46:51 +0200

    Added LaserSight Skill!

---
+++ WeAreTheVillianLTS/Assets/LaserEyesSkill.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserEyesSkill : AttackSkill
+{
+    [SerializeField] LayerMask enemyLayer;
+    [SerializeField] float laserRange;
+    [SerializeField] int laserDmg;
+    RaycastHit2D hit;
+    public override void UseSkill()
+    {
+        if (coolDownCounter > 0) return;
+        coolDownCounter = settings.CoolDown;
+        Debug.DrawRay(transform.position, transform.right*laserRange, Color.red, 1f);
+
+        hit = Physics2D.Raycast(transform.position, transform.right, laserRange, enemyLayer);
+        if (hit.collider != null)
+        {
+            hit.collider.GetComponent<BaseDamagable>().TakeDamage(laserDmg);
+        }
+    }
+}
